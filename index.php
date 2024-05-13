c9dd2e4ebd541f8a9a8c1156e88fa500b4c28d5e
Kredo-Garry <105405260+Kredo-Garry@users.noreply.github.com>
2024-05-13 21:05:46 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Fare Activity</title>
+</head>
+<body>
+    <form action="" method="post">
+        <input type="number" name="age" placeholder="Age" min="10" max="80"><br>
+        <input type="number" name="distance" placeholder="Distance (km)" min="1"><br>
+        <button type="submit" name="btn_compute">Compute</button>
+    </form>
+</body>
+</html>
+
+<?php
+include_once "fare.php"; //more strict -- only include the file one time
+//include "fare.php"; //not so strict -- will always include the file everytime the program run
+
+if(isset($_POST["btn_compute"])){
+    $transpo = new Fare;
+
+    $transpo->setAge($_POST['age']);
+    $transpo->setDistance($_POST['distance']);
+    $transpo->setFare();
+
+    echo "Age: " .$transpo->getAge(). " years old<br>";
+    echo "Distance: " .$transpo->getDistance(). " km<br>";
+    echo "Fare: " .$transpo->getFare();
+}
+
+?>
